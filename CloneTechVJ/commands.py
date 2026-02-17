85a1d3014ea14aa9584110b9b5b07909585579a3
sameer9835 <sameer25072006@gmail.com>
2026-02-18 00:36:35 +0530

    Update commands.py

--- CloneTechVJ/commands.py
+++ CloneTechVJ/commands.py
@@ -232,11 +232,16 @@ async def reset_settings(client, message):
         await db.update_bot(me.id, data)
         await message.reply("**Successfully Reset All Settings To Default.**")
 
-@Client.on_message(filters.command("stats") & filters.private & filters.user(ADMINS))
+@Client.on_message(filters.command("stats") & filters.private)
 async def stats(client, message):
+
+    if message.from_user.id not in ADMINS:
+        return await message.reply("You are not authorized to use this command.")
+
     me = await client.get_me()
     total_users = await clonedb.total_users_count(me.id)
     filesp = col.count_documents({})
     totalsec = sec_col.count_documents({})
     total = int(filesp) + int(totalsec)
+
     await message.reply(f"**Total Files : {total}\n\nTotal Users : {total_users}**")
