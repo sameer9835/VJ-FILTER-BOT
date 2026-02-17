7136e6093e5cbece154aa4682666384b9ac62b06
sameer9835 <sameer25072006@gmail.com>
2026-02-17 22:43:08 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -201,17 +201,17 @@ Search on ʏᴏᴜʀ own..."""
 ❌ No results found for :
 ╰┈➤ {} 
 
+╰┈➤ 👤 Searched By ➜ {}
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 📝 Please check the following ➜
 𝟏 ➤ Make sure Spelling is correct
 
 𝟐 ➤ Just Type only: (Title) or (Author name) or BOTH
-╰➤ Do NOT add extra words or symbols
 ╰➤ Example: Atomic Habits
-       OR: Atomic Habits James Clear
+    OR: Atomic Habits James Clear
 
-𝟑 ➤ If it still does not appear,
-Go to (Book Request Topics) & WRITE :
-✅ Complete ( TITLE + AUTHOR NAME )
+𝟑 ➤ If it still does not appear:
+    Go to (Request Topics) & WRITE: ( TITLE + AUTHOR NAME )
 </b>"""
 
     I_CUD_NT = """ɪ ᴄᴏᴜʟᴅɴ'ᴛ ꜰɪɴᴅ ᴀɴʏ Book ʀᴇʟᴀᴛᴇᴅ ᴛᴏ {}.
@@ -756,6 +756,7 @@ Example:- /set_caption 📕 File Name: {filename}
 
 
     
+
 
 
 
